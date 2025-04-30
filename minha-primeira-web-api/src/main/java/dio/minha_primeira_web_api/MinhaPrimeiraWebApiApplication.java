359101e1716b03204a5efb7c3285e323b5eb4088
Jackson Sales <jacksonmatheus.sales@gmail.com>
2025-04-30 14:08:05 -0300

    Testando Modificacoes no properties

--- minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/MinhaPrimeiraWebApiApplication.java
+++ minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/MinhaPrimeiraWebApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+@EnableFeignClients(basePackages = "dio.minha_primeira_web_api")
 @SpringBootApplication
 public class MinhaPrimeiraWebApiApplication {
 
