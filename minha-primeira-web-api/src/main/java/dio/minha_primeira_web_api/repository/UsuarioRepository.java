94555f2e6ee8d253c9ede3b734e519af04e59961
Jackson Sales <jacksonmatheus.sales@gmail.com>
2025-05-01 17:11:26 -0300

    add docker image

--- minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/repository/UsuarioRepository.java
+++ minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/repository/UsuarioRepository.java
@@ -1,13 +1,13 @@
-//package dio.minha_primeira_web_api.repository;
-//
-//import dio.minha_primeira_web_api.model.Usuario;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.Optional;
-//
-//
-//@Repository
-//public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
-//    Optional<Usuario> findByUsername(String username);
-//}
+package dio.minha_primeira_web_api.repository;
+
+import dio.minha_primeira_web_api.model.Usuario;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+}
--- minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/service/UsuarioService.java
+++ minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/service/UsuarioService.java
@@ -1,20 +1,20 @@
-//package dio.minha_primeira_web_api.service;
-//
-//import dio.minha_primeira_web_api.model.Usuario;
-//import org.springframework.stereotype.Service;
-//
-//
-//public interface UsuarioService {
-//    Iterable<Usuario> buscarTodos();
-//
-//    Usuario buscarPorId(Long id);
-//
-//    void inserir(Usuario usuario);
-//
-//    void atualizar(Long id, Usuario usuario);
-//
-//    void atualizarSenha(Long id, String senhaAntiga, String senhaNova);
-//
-//    void deletar(Long id);
-//
-//}
+package dio.minha_primeira_web_api.service;
+
+import dio.minha_primeira_web_api.model.Usuario;
+import org.springframework.stereotype.Service;
+
+
+public interface UsuarioService {
+    Iterable<Usuario> buscarTodos();
+
+    Usuario buscarPorId(Long id);
+
+    void inserir(Usuario usuario);
+
+    void atualizar(Long id, Usuario usuario);
+
+    void atualizarSenha(Long id, String senhaAntiga, String senhaNova);
+
+    void deletar(Long id);
+
+}
