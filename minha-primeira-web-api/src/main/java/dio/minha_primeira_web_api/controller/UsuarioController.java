f9dc23d5ad8d15892110b3813d2912d537b0e607
Jackson Sales <jacksonmatheus.sales@gmail.com>
2025-05-01 13:44:49 -0300

    teste

--- minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/controller/UsuarioController.java
+++ minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/controller/UsuarioController.java
@@ -1,33 +1,33 @@
-package dio.minha_primeira_web_api.controller;
-
-import dio.minha_primeira_web_api.model.SenhaDTO;
-import dio.minha_primeira_web_api.model.Usuario;
-import dio.minha_primeira_web_api.service.UsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/users")
-public class UsuarioController {
-    @Autowired
-    private UsuarioService usuarioService;
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePorId(@PathVariable("id") Long id){
-        usuarioService.deletar(id);
-        return ResponseEntity.ok().build();
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuario){
-        usuarioService.atualizar(id, usuario);
-        return ResponseEntity.ok(usuario);
-    }
-
-    @PutMapping("/{id}/password")
-    public ResponseEntity<Void> atualizarSenha(@PathVariable Long id, @RequestBody SenhaDTO senhaDTO) {
-        usuarioService.atualizarSenha(id, senhaDTO.getSenhaAntiga(), senhaDTO.getSenhaNova());
-        return ResponseEntity.ok().build();
-    }
-}
+//package dio.minha_primeira_web_api.controller;
+//
+//import dio.minha_primeira_web_api.model.SenhaDTO;
+//import dio.minha_primeira_web_api.model.Usuario;
+//import dio.minha_primeira_web_api.service.UsuarioService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/users")
+//public class UsuarioController {
+//    @Autowired
+//    private UsuarioService usuarioService;
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Void> deletePorId(@PathVariable("id") Long id){
+//        usuarioService.deletar(id);
+//        return ResponseEntity.ok().build();
+//    }
+//
+//    @PutMapping("/{id}")
+//    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuario){
+//        usuarioService.atualizar(id, usuario);
+//        return ResponseEntity.ok(usuario);
+//    }
+//
+//    @PutMapping("/{id}/password")
+//    public ResponseEntity<Void> atualizarSenha(@PathVariable Long id, @RequestBody SenhaDTO senhaDTO) {
+//        usuarioService.atualizarSenha(id, senhaDTO.getSenhaAntiga(), senhaDTO.getSenhaNova());
+//        return ResponseEntity.ok().build();
+//    }
+//}
--- minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/repository/EnderecoRepository.java
+++ minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/repository/EnderecoRepository.java
@@ -1,8 +1,8 @@
-package dio.minha_primeira_web_api.repository;
-
-import dio.minha_primeira_web_api.model.Endereco;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EnderecoRepository extends CrudRepository<Endereco, String> {
-
-}
+//package dio.minha_primeira_web_api.repository;
+//
+//import dio.minha_primeira_web_api.model.Endereco;
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+//
+//}
