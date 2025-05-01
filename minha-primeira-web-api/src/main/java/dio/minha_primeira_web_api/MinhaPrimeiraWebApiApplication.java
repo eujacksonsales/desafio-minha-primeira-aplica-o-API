c659259b37fb59c1493626cd693a57707f735ec7
Jackson Sales <jacksonmatheus.sales@gmail.com>
2025-05-01 14:16:56 -0300

    teste

--- minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/MinhaPrimeiraWebApiApplication.java
+++ minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/MinhaPrimeiraWebApiApplication.java
@@ -2,13 +2,13 @@ package dio.minha_primeira_web_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackages = "dio.minha_primeira_web_api")
+//@EnableFeignClients(basePackages = "dio.minha_primeira_web_api")
 @SpringBootApplication
 public class MinhaPrimeiraWebApiApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.context.parallel", "true");
 		SpringApplication.run(MinhaPrimeiraWebApiApplication.class, args);
 	}
 
