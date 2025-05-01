0e75e16ef2ff70df0700036b12e68d269a26a933
Jackson Sales <jacksonmatheus.sales@gmail.com>
2025-05-01 12:35:05 -0300

    fix: /health

--- minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/controller/HealthController.java
+++ minha-primeira-web-api/src/main/java/dio/minha_primeira_web_api/controller/HealthController.java
@@ -1,19 +1,15 @@
 package dio.minha_primeira_web_api.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping
 public class HealthController {
 
-    @GetMapping("/")
+    @GetMapping("/health")
     @ResponseBody
     public String home() {
         return "Aplicação no ar ✅";
